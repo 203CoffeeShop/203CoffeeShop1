6c9e8bcccc7c417352ee0e1044a7a636a96e96f4
RazzieBoy <97880341+RazzieBoy@users.noreply.github.com>
2023-11-06 15:15:38 +1300

    Working on cart

--- static/addToOrder.js
+++ static/addToOrder.js
@@ -41,3 +41,13 @@ var coffeePopularSpan = document.createElement("span");
                 alert("Price: $" + coffeePrice.toFixed(2));
             });
         }
+
+
+  /* Close */
+  function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+  }
+  /* Open */
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+  }
